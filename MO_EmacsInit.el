d4613c99bf899941d08756a3e486191dfe143b9e
mozcelikors <mozcelikors@gmail.com>
2017-09-01 20:50:37 +0200

    Edited emacs lisp file
    Signed-off-by: mozcelikors <mozcelikors@gmail.com>

--- MO_EmacsInit.el
+++ MO_EmacsInit.el
@@ -2,21 +2,21 @@
 ;;  @author mozcelikors <mozcelikors@gmail.com>
 
 (custom-set-variables
-	;; custom-set-variables was added by Custom.
-	;; If you edit it by hand, you could mess it up, so be careful.
-	;; Your init file should contain only one such instance.
-	;; If there is more than one, they won't work right.
-	'(ansi-color-faces-vector
-	[default default default italic underline success warning error])
-	'(ansi-color-names-vector
-	["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
-	'(custom-enabled-themes (quote (wombat))))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
+ '(custom-enabled-themes (quote (wombat))))
 (custom-set-faces
-	;; custom-set-faces was added by Custom.
-	;; If you edit it by hand, you could mess it up, so be careful.
-	;; Your init file should contain only one such instance.
-	;; If there is more than one, they won't work right.
-)
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 ;; Maximize GNU Emacs on start-up
 ;;(add-to-list 'default-frame-alist '(fullscreen . maximized))
@@ -116,7 +116,7 @@
 (let ((default-directory "/home/mozcelikors/"))
 	(shell "*shell"))
 
-(let ((default-directory "/media/sf_Ubuntu_Shared"))
+(let ((default-directory "/media/sf_Ubuntu_Shared/MyGitHub/"))
 	(shell "*shell1"))
 
 (let ((default-directory "/home/mozcelikors/"))
